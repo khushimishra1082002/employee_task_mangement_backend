eb673038d3a31a0df1acb0caeb8fdb47e9b01817
nrtdev <dev@newrise.in>
2026-01-12 21:03:19 +0530

    more changes with server

--- index.js
+++ index.js
@@ -33,11 +33,15 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/admin/authRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
 
 // test route
 app.get("/", (req, res) => {
