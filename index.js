8238eb8ea4ed939d467d1fc8caed96e7f83e5ae5
nrtdev <dev@newrise.in>
2026-01-12 20:44:45 +0530

    more changes with index

--- index.js
+++ index.js
@@ -34,22 +34,10 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
-const authRoutes = require("./routes/admin/authRoutes");
-const userRoutes = require("./routes/admin/userRoutes");
-const taskRoutes = require("./routes/admin/taskRoutes");
-const employeeRoutes = require("./routes/employee/employeeRoutes");
-const profileRoutes = require("./routes/profileRoutes");
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/api/auth", authRoutes);
-app.use("/api/user", userRoutes);
-app.use("/api/task", taskRoutes);
-app.use("/api/employeeTask", employeeRoutes);
-app.use("/api/profile", profileRoutes);
 
 // test route
 app.get("/", (req, res) => {
