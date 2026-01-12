c19966b81f47b0bdd5007e2c930e6fab71e8263b
nrtdev <dev@newrise.in>
2026-01-12 11:01:17 +0530

    six comit

--- src/config/db.js
+++ src/config/db.js
@@ -11,9 +11,34 @@
 // };
 
 // module.exports = connectDB;
+// const mongoose = require("mongoose");
+
+// let cached = global.mongoose; // reuse global connection in serverless env
+
+// if (!cached) {
+//   cached = global.mongoose = { conn: null, promise: null };
+// }
+
+// async function connectDB() {
+//   if (cached.conn) {
+//     return cached.conn; // reuse existing connection
+//   }
+
+//   if (!cached.promise) {
+//     cached.promise = mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     }).then((mongoose) => mongoose);
+//   }
+
+//   cached.conn = await cached.promise;
+//   return cached.conn;
+// }
+
+// module.exports = connectDB;
 const mongoose = require("mongoose");
 
-let cached = global.mongoose; // reuse global connection in serverless env
+let cached = global.mongoose;
 
 if (!cached) {
   cached = global.mongoose = { conn: null, promise: null };
@@ -21,14 +46,12 @@ if (!cached) {
 
 async function connectDB() {
   if (cached.conn) {
-    return cached.conn; // reuse existing connection
+    return cached.conn;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(process.env.MONGO_URI)
+      .then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
