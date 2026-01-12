3db23ad2fffa1c992c7eef0bb4558ae8b8a11f60
nrtdev <dev@newrise.in>
2026-01-12 19:55:24 +0530

    changes with file structure

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/admin/authRoutes");
+const userRoutes = require("./routes/admin/userRoutes");
+const taskRoutes = require("./routes/admin/taskRoutes");
+const employeeRoutes = require("./routes/employee/employeeRoutes");
+const profileRoutes = require("./routes/profileRoutes");
+require("dotenv").config();
+
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+app.use("/uploads", express.static("uploads"));
+
+connectDB()
+  .then(() => console.log("database connceted"))
+  .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/task", taskRoutes);
+app.use("/api/employeeTask", employeeRoutes);
+app.use("/api/profile", profileRoutes);
+
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+module.exports = app
--- server.js
+++ server.js
@@ -7,35 +7,42 @@
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const express = require("express");
-const cors = require("cors");
+// const express = require("express");
+// const cors = require("cors");
 
-const app = express();
+// const app = express();
 
-const connectDB = require("./src/config/db");
+// const connectDB = require("./config/db");
 
-app.use(cors());
-app.use(express.json());
+// app.use(cors());
+// app.use(express.json());
 
-connectDB().then(() =>
-  console.log("database connceted")
-).catch((err) => console.log(err))
+// connectDB().then(() =>
+//   console.log("database connceted")
+// ).catch((err) => console.log(err))
 
-app.get("/", (req, res) => {
-  res.send("employee");
-});
+// app.get("/", (req, res) => {
+//   res.send("employee");
+// });
 
-app.use("/api/auth", require("./src/routes/admin/authRoutes"));
-app.use("/api/user", require("./src/routes/admin/userRoutes"));
-app.use("/api/task", require("./src/routes/admin/taskRoutes"));
-app.use("/api/employeeTask", require("./src/routes/employee/employeeRoutes"));
-app.use("/api/profile", require("./src/routes/profileRoutes"));
+// app.use("/api/auth", require("./src/routes/admin/authRoutes"));
+// app.use("/api/user", require("./src/routes/admin/userRoutes"));
+// app.use("/api/task", require("./src/routes/admin/taskRoutes"));
+// app.use("/api/employeeTask", require("./src/routes/employee/employeeRoutes"));
+// app.use("/api/profile", require("./src/routes/profileRoutes"));
 
-app.use("/uploads", express.static("uploads"));
+// app.use("/uploads", express.static("uploads"));
 
-// Catch-all 404 route (important!)
-app.use((req, res) => {
-  res.status(404).json({ success: false, message: "Route not found ❌" });
-});
+// // Catch-all 404 route (important!)
+// app.use((req, res) => {
+//   res.status(404).json({ success: false, message: "Route not found ❌" });
+// });
 
-module.exports = app;
+// module.exports = app;
+
+const app = require("./index")
+require("dotenv").config();
+
+
+const PORT = process.env.PORT || 5000
+ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
