c573cc01efdb74df16ff0f18d2b38552e835b1ec
nrtdev <dev@newrise.in>
2026-01-12 21:08:30 +0530

    change in auth route

--- routes/admin/authRoutes.js
+++ routes/admin/authRoutes.js
@@ -1,20 +1,30 @@
+// const express = require("express");
+// const router = express.Router();
+// const {
+//   registerController,
+//   loginController,
+//   changePasswordController
+// } = require("../../controller/authController");
+// const upload = require("../../middleware/multerMiddleware")
+// const authtoken = require("../../middleware/authMiddleware");
+
+// // REGISTER
+// router.post("/register", upload.single("image"), registerController);
+
+// // LOGIN
+// router.post("/login", loginController);
+
+// // 🔐 Change password (protected)
+// router.post("/change-password", authtoken, changePasswordController);
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
-const {
-  registerController,
-  loginController,
-  changePasswordController
-} = require("../../controller/authController");
-const upload = require("../../middleware/multerMiddleware")
-const authtoken = require("../../middleware/authMiddleware");
 
-// REGISTER
-router.post("/register", upload.single("image"), registerController);
-
-// LOGIN
-router.post("/login", loginController);
-
-// 🔐 Change password (protected)
-router.post("/change-password", authtoken, changePasswordController);
+router.get("/test", (req, res) => {
+  res.send("Auth route working");
+});
 
 module.exports = router;
+
