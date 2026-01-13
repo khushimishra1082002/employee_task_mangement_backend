ff545b2515954038f33cf553d81614d0c033b9d7
nrtdev <dev@newrise.in>
2026-01-13 20:35:08 +0530

    canges in user route

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/admin/authRoutes");
+const userRoutes = require("./routes/admin/userRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const taskRoutes = require("./routes/admin/taskRoutes");
 
@@ -43,6 +44,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
 app.use("/api/profile", profileRoutes);
 app.use("/api/task", taskRoutes);
 
